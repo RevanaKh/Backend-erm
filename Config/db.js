736259db70124ef188b6e3cd9e1f73d5cfc961d5
Revana Khoerunnisa <revana.khoerunnisa38@sma.belajar.id>
2025-06-13 00:46:35 +0700

    perbaiki db

--- Config/db.js
+++ Config/db.js
@@ -1,12 +1,12 @@
-require('dotenv').config(); // pastikan kamu install dan pakai dotenv kalau pakai .env
+require('dotenv').config();
 
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST,
   user: 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'ERM',
+  database: process.env.DB_NAME ,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
