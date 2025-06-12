110612bee385cf299f7185d61259dbbe7d96f051
Revana Khoerunnisa <revana.khoerunnisa38@sma.belajar.id>
2025-06-13 00:54:10 +0700

    db

--- Config/db.js
+++ Config/db.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST|| 'switchyard.proxy.rlwy.net',
   user: 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD ,
   database: process.env.DB_NAME ,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   waitForConnections: true,
